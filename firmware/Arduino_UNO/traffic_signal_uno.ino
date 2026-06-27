c3889610e3b6d19c36c7db18f409d65f12ebdd7a
JanaviSadamke477 <146927088+JanaviSadamke477@users.noreply.github.com>
2026-06-27 21:26:28 +0530

    Update traffic_signal_uno.ino

--- firmware/Arduino_UNO/traffic_signal_uno.ino
+++ firmware/Arduino_UNO/traffic_signal_uno.ino
@@ -1,1 +1,18 @@
+#include "pins.h"
+#include "traffic_light.h"
+#include "servo_control.h"
 
+void setup()
+{
+    initializePins();
+    initializeServo();
+}
+
+void loop()
+{
+    normalTrafficCycle();
+
+    checkLane1();
+
+    checkLane2();
+}
