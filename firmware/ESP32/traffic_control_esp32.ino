7397b940ae57e9532cd945bc7b1fff3b2d391107
JanaviSadamke477 <146927088+JanaviSadamke477@users.noreply.github.com>
2026-06-27 21:16:26 +0530

    Update traffic_control_esp32.ino

--- firmware/ESP32/traffic_control_esp32.ino
+++ firmware/ESP32/traffic_control_esp32.ino
@@ -1,1 +1,27 @@
+#include "config.h"
 
+void setup()
+{
+    Serial.begin(115200);
+
+    setupPins();
+
+    connectWiFi();
+
+    connectBlynk();
+
+    initializeSystem();
+}
+
+void loop()
+{
+    Blynk.run();
+
+    timer.run();
+
+    detectSiren();
+
+    checkEmergency();
+
+    updateDashboard();
+}
